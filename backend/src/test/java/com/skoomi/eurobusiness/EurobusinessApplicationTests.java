715e1e5dc06a5feeedd2f7cb589749057e9fa8fd
Michał Skowron <michal.skowron92@gmail.com>
2024-06-02 14:43:06 +0200

    Init backendu na springboot

---
+++ backend/src/test/java/com/skoomi/eurobusiness/EurobusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skoomi.eurobusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurobusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
